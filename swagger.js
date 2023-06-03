028b5f712ad6761ae52ab45440efaa7552860ecf
unknown <mazayw@gmail.com>
2023-06-03 18:20:40 +0300

    fix: swagger url

--- swagger.js
+++ swagger.js
@@ -9,9 +9,8 @@ const options = {
 			contact: {
 				name: 'Ihor',
 				Discord: '@Mazayw#8624',
-				//url: 'https://github.com/DesmondSanctity/node-js-swagger',
 			},
-			version: '1.0.0',
+			version: '1.0.2',
 		},
 		components: {
 			securitySchemes: {
@@ -21,23 +20,20 @@ const options = {
 		security: [{ bearerAuth: [] }],
 		servers: [
 			{
+				url: 'https://baza-trainee-7ain.onrender.com/',
+				description: 'Live server',
+			},
+			{
 				url: 'http://localhost:3000/',
 				description: 'Local server',
 			},
-			{
-				url: 'https://baza-trainee.onrender.com',
-				description: 'Live server',
-			},
 		],
 	},
-	// looks for configuration in specified directories
 	apis: ['./routes/*.js', './models/*.js'],
 };
 const swaggerSpec = swaggerJsdoc(options);
 function swaggerDocs(app, port) {
-	// Swagger Page
 	app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-	// Documentation in JSON format
 	app.get('/docs/api.json', (req, res) => {
 		res.setHeader('Content-Type', 'application/json');
 		res.send(swaggerSpec);
