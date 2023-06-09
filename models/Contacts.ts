ddf2659e4fc7195389af978578cf5ac16d02b0e1
unknown <mazayw@gmail.com>
2023-06-09 16:50:48 +0300

    feat: add contacts model

---
+++ models/Contacts.ts
@@ -0,0 +1,65 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Contacts:
+ *       type: object
+ *       properties:
+ *         contacts:
+ *           type: object
+ *           properties:
+ *             contactsDataList:
+ *               type: object
+ *               properties:
+ *                 phone1:
+ *                   type: number
+ *                   example: 1234567890
+ *                 phone2:
+ *                   type: number
+ *                   example: 9876543210
+ *                 email:
+ *                   type: string
+ *                   example: example@example.com
+ *             socialsMediaList:
+ *               type: object
+ *               properties:
+ *                 linkedin:
+ *                   type: string
+ *                   example: https://www.linkedin.com/in/example
+ *                 facebook:
+ *                   type: string
+ *                   example: https://www.facebook.com/example
+ */
+
+import mongoose from 'mongoose';
+
+const ContactsSchema = new mongoose.Schema({
+	contacts: {
+		contactsDataList: {
+			phone1: {
+				type: Number,
+				required: true,
+			},
+			phone2: {
+				type: Number,
+				required: true,
+			},
+			email: {
+				type: String,
+				required: true,
+			},
+		},
+		socialsMediaList: {
+			linkedin: {
+				type: String,
+				required: true,
+			},
+			facebook: {
+				type: String,
+				required: true,
+			},
+		},
+	},
+});
+
+export default mongoose.model('Contacts', ContactsSchema);
