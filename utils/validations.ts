cdcc9c9afee949465e7dcc15af7ed639c1a1994d
unknown <mazayw@gmail.com>
2023-06-13 01:28:40 +0300

    fix: role validation

--- utils/validations.ts
+++ utils/validations.ts
@@ -71,10 +71,27 @@ export const projectCreateValidation = [
 ];
 
 export const roleCreateValidation = [
-	body(
-		'name',
-		'The name is incorrect, it must contain more than 2 character'
-	).isLength({ min: 2 }),
+	body('name.en')
+		.optional()
+		.isString()
+		.isLength({ min: 5 })
+		.withMessage(
+			'The en name is incorrect, it must contain more than 5 characters'
+		),
+	body('name.pl')
+		.optional()
+		.isString()
+		.isLength({ min: 5 })
+		.withMessage(
+			'The pl name is incorrect, it must contain more than 5 characters'
+		),
+	body('name.ua')
+		.optional()
+		.isString()
+		.isLength({ min: 5 })
+		.withMessage(
+			'The ua name is incorrect, it must contain more than 5 characters'
+		),
 	(req: Request, res: Response, next: () => void) =>
 		checkValidation(req, res, next),
 ];
