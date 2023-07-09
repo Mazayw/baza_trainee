00e22f472c30d9cb81a325c1385c9970b891abff
unknown <mazayw@gmail.com>
2023-07-09 12:31:14 +0300

    feat: add update script file

---
+++ update-and-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /app/baza/baza_trainee_back
+
+git fetch
+
+if git diff --quiet origin/dev; then
+  echo "No changes on the remote branch."
+else
+  git pull origin dev
+
+  make build
+fi
