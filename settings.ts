df154ffd21687044ea4f0eb6d554070aaea13c86
Ivan <46397494+Simkav@users.noreply.github.com>
2024-08-28 15:14:12 +0300

    upd file size limits

--- settings.ts
+++ settings.ts
@@ -4,7 +4,7 @@ export const SETTINGS = {
     // The maximum file size in bytes (e.g., 1 * 1024 * 1024 = 1MB)
     partnerLogo: 500 * 1024,
     projectCard: 1 * 1024 * 1024,
-    report: 500 * 1024,
+    report: 5 * 1024 * 1024,
     testimonialPhoto: 500 * 1024,
     heroSliderPhoto: 1 * 1024 * 1024,
     articleImage: 500 * 1024,
