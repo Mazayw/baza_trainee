8d3ad77c3703f94c9b1ca9659269024b24615cc1
Ivan <46397494+Simkav@users.noreply.github.com>
2024-11-01 18:39:09 +0300

    fix mail reciver

--- controllers/v2/feedbackController.ts
+++ controllers/v2/feedbackController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { sendMail } from '../../utils/email/nodemailer';
 
+const mailReciver = String(process.env.SMTP_FROM)
+
 export const sendFeedback = async (req: Request, res: Response) => {
   try {
     const sendEmailDTO = req.body;
@@ -10,7 +12,7 @@ email: ${sendEmailDTO.email}
 text : ${sendEmailDTO.text}
 `;
     const subject = `Baza-trainee feedback form`;
-    await sendMail(text, subject, 'mail@misto-fest.fun');
+    await sendMail(text, subject, mailReciver);
     return res.json({ status: true });
   } catch (error) {
     console.error('Error retrieving contacts data', error);
@@ -34,7 +36,7 @@ specialization: ${sendEmailDTO.specialization}
 convenient_time: ${sendEmailDTO.convenient_time}
 `;
     const subject = `Baza-trainee mentor form`;
-    await sendMail(text, subject, 'mail@misto-fest.fun');
+    await sendMail(text, subject, mailReciver);
     return res.json({ status: true });
   } catch (error) {
     console.error('Error retrieving contacts data', error);
